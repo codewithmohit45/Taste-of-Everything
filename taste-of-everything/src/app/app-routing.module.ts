378cf080c185a89c74e908737fd7423bf74e74a0
codewithmohit <jindalmohit2018@gmail.com>
2021-09-24 15:43:13 +0530

    update project

--- taste-of-everything/src/app/app-routing.module.ts
+++ taste-of-everything/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { ItemComponent } from './item/item.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
+  { path: '', component: CategoryComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'item', component: ItemComponent },
+
 ];
 
 @NgModule({
--- taste-of-everything/src/app/app.component.ts
+++ taste-of-everything/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taste-of-everything';
+
+  flag: boolean;
+
 }
