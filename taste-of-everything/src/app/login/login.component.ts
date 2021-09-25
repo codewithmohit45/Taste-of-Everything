f6c68163f55ae76d5fbf65f0de8cc4f307965932
codewithmohit <jindalmohit2018@gmail.com>
2021-09-25 10:46:19 +0530

    Login and Signup

--- taste-of-everything/src/app/login/login.component.ts
+++ taste-of-everything/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Customer } from '../customer';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  customer: Customer;
 
-  constructor() { }
+  constructor(private service: CustomerService, private router: Router) {
+    this.customer = new Customer();
+  }
 
   ngOnInit() {
+  }
+
+  public checkValidCustomer() {
+
+    if (this.service.checkCustomer(this.customer)) {
+      this.router.navigate([""]);
+    } else {
+      this.router.navigate(["/signup"]);
+    }
   }
 
 }
